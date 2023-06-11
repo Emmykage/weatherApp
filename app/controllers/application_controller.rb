d44e699c06f27a933649f59f9e38c2c1df64f134
morris menanya <emmiemenz@gmail.com>
2023-06-11 13:47:44 +0100

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,4 @@
+class ApplicationController < ActionController::Base
+
+   @locations = [{id: 1, city: 'abuja'}, {id: 2, city: 'london'}, {id: 3, city: 'nairobi'},{id: 4, city: 'new york'}, {id: 5, city: "Abu Dhabi"}]
+end
